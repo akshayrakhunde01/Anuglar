ae007e4deb38f658f6256bd5c67aff6261c90a43
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-03-24 22:36:15 +0530

    add structural directive component with template, styles, and tests

---
+++ src/app/components/structural-directive/structural-directive.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directive',
+  imports: [],
+  templateUrl: './structural-directive.component.html',
+  styleUrl: './structural-directive.component.css'
+})
+export class StructuralDirectiveComponent {
+
+}
