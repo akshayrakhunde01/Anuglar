adc14141647d4325f764c322a5c8ca65585ec949
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-03-25 00:11:23 +0530

    add watching state management to structural directive component

--- src/app/components/structural-directive/structural-directive.component.ts
+++ src/app/components/structural-directive/structural-directive.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-structural-directive',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './structural-directive.component.html',
   styleUrl: './structural-directive.component.css'
 })
 export class StructuralDirectiveComponent {
+  isWatching:boolean=true;
 
+  constructor() { 
+   
+  }
+   hideWatching(){
+      this.isWatching=false
+  }
+  showWatching(){
+    this.isWatching=true
+  }
 }
