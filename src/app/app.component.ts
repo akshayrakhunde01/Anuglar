413663ac457cda5119f7a10ab6988d2eb8b05465
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-03-31 23:48:06 +0530

    Add attribute directive component to app and update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
+import { AttributeDirectiveComponent } from './components/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,DatabindingComponent,StructuralDirectiveComponent],
+  imports: [RouterOutlet,DatabindingComponent,StructuralDirectiveComponent,AttributeDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
