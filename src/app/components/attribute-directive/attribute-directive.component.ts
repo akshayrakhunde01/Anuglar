ee56850258df1b58e5a8735ba5c0d32e9da41628
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-01 23:33:56 +0530

    Enhance attribute directive component with new HTML structure and color change methods

--- src/app/components/attribute-directive/attribute-directive.component.ts
+++ src/app/components/attribute-directive/attribute-directive.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attribute-directive',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attribute-directive.component.html',
   styleUrl: './attribute-directive.component.css'
 })
 export class AttributeDirectiveComponent {
+ constructor(){}
 
+ chanageColor:string=''
+ 
+ changeColorDiv1TOGreen(){
+  this.chanageColor='green'
+ }
+
+ changeColorDiv1TOYellow(){
+  this.chanageColor='yellow'
+ }
 }
