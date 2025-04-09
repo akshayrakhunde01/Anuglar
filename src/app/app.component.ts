f43941839c9a11319ccf149ff04e0cccfcadb1ec
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-09 23:43:21 +0530

    Add navigation links to the app component and update imports for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
 import { AttributeDirectiveComponent } from './components/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   // DatabindingComponent,StructuralDirectiveComponent,AttributeDirectiveComponent
