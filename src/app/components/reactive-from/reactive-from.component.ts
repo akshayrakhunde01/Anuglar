bf4b739fe30ba408f188250aa8ecaec6d2ffed9a
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-11 00:37:59 +0530

    Add ReactiveFromComponent with HTML, CSS, and unit tests

---
+++ src/app/components/reactive-from/reactive-from.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-from',
+  imports: [],
+  templateUrl: './reactive-from.component.html',
+  styleUrl: './reactive-from.component.css'
+})
+export class ReactiveFromComponent {
+
+}
