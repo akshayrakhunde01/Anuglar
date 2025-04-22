722373b1ab46babe02445cd6411fd7c3a85b3213
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-22 23:18:25 +0530

    Remove unused import for StructuralDirectiveComponent in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 
 import { Routes } from '@angular/router';
-import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
 import { AttributeDirectiveComponent } from './components/attribute-directive/attribute-directive.component';
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { TemplateFromComponent } from './components/template-from/template-from.component';
