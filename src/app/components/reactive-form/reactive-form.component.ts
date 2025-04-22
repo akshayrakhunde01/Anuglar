9b29eb9011dd4123e745891c4fd4be1ebc318d01
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-23 00:33:22 +0530

    Refactor ReactiveFormComponent to include form controls for first name, last name, and address; update template for improved user input handling.

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -3,8 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  // imports: [ReactiveFormsModule],
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css',
 })
@@ -20,17 +19,16 @@ export class ReactiveFormComponent implements OnInit {
     this.registrationForm = new FormGroup({
       firstName: new FormControl(''),
       lastName: new FormControl(''),
-      email:new FormControl(null),
-      username:new FormControl(''),
-      DateOfBirth:new FormControl(''),
+      email: new FormControl(null),
+      username: new FormControl(''),
+      dateOfBirth: new FormControl(''),
       gender: new FormControl(''),
-      streetAddress:new FormControl(''),
-      country:new  FormControl(''),
-      city:new FormControl(''),
-      region:new FormControl(''),
-      postalCode:new FormControl(''),
-      createAuserName:new FormControl(''),
-      submit:new FormControl('')
+      streetAddress: new FormControl(''),
+      country: new FormControl(''),
+      city: new FormControl(''),
+      region: new FormControl(''),
+      postalCode: new FormControl('')
     });
   }
+  
 }
