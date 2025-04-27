58165ece05ca3ef97bd311dacb408138631cb62d
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-28 01:20:48 +0530

    Add route for ReactiveFormValidationComponent in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { AttributeDirectiveComponent } from './components/attribute-directive/at
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { TemplateFromComponent } from './components/template-from/template-from.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { ReactiveFormValidationComponent } from './components/reactive-form-validation/reactive-form-validation.component';
 
 export const routes: Routes = [
     {path:"DatabindingComponent", component:DatabindingComponent},
     {path:'attribute', component:AttributeDirectiveComponent},
     {path:'template-form',component:TemplateFromComponent},
-    {path:'reactive-form',component:ReactiveFormComponent}
+    {path:'reactive-form',component:ReactiveFormComponent},
+    {path:'reactive-form-Validation',component:ReactiveFormValidationComponent}
 ];
