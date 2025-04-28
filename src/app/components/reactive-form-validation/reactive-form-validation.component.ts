23fa0afb0535399fb2b6ba11d72d930619e79ec3
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-04-29 00:21:36 +0530

    Implement reactive form with validation for user input fields in ReactiveFormValidationComponent

--- src/app/components/reactive-form-validation/reactive-form-validation.component.ts
+++ src/app/components/reactive-form-validation/reactive-form-validation.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { fakeAsync } from '@angular/core/testing';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-validation',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './reactive-form-validation.component.html',
   styleUrl: './reactive-form-validation.component.css'
 })
 export class ReactiveFormValidationComponent {
+  userForm:FormGroup
 
+  constructor(){
+    this.userForm=new FormGroup({
+      firstName:new FormControl("",[Validators.required]),
+      lastName:new FormControl(""),
+      userName: new FormControl(""),
+      city:new FormControl(""),
+      state:new FormControl("Maharashtra"),
+      zip:new FormControl(""),
+      agreement:new FormControl(false)
+    })
+  }
+
+  onSubmit(){
+  //  alert('form submit event triggerd')
+  }
 }
